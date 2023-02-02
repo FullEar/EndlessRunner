13b4b5c84dd361e8e34adffe8c3d6dd7207d0162
Eduardo <98367885+FullEar@users.noreply.github.com>
2023-02-02 04:03:26 -0600

    New Stuff
    Daño, Musica, Colisiones y mas-

--- Endless Runner/Assets/DamageScript.cs
+++ Endless Runner/Assets/DamageScript.cs
@@ -7,10 +7,20 @@ public class DamageScript : MonoBehaviour
     public int damage;
     float cooldown = 0f;
 
-    private void OnCollisionEnter(Collision collision)
+   
+    //private void OnCollisionEnter(Collision collision)
+   // {
+        //HealthScript health = collision.gameObject.GetComponent<HealthScript>();
+        //if (health != null)
+        //{
+         //   health.TakeDamage(damage);
+        //}
+   // }
+    
+    private void OnTriggerEnter(Collider other)
     {
-        HealthScript health = collision.gameObject.GetComponent<HealthScript>();
-        if (health != null)
+        HealthScript health = other.gameObject.GetComponent<HealthScript>();
+        if(health != null)
         {
             health.TakeDamage(damage);
         }
